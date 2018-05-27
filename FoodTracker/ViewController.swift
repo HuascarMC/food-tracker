b05322f57a171a38a8226c7e0348dd4537a5f787
Huascar <huascarmc@hotmail.com>
2018-05-27 19:09:02 +0100

    added link textField/textLabel

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var labelTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
