e723753a115075bbab206fbbc96171e7757617fc
Huascar <huascarmc@hotmail.com>
2018-05-29 14:17:50 +0100

    added initializer methods to rating control

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -2,4 +2,14 @@ import UIKit
 
 class RatingControl: UIStackView {
     
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        super.init(frame: frame)
+    }
+    
 }
