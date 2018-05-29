014f10454f520641a266066a055d50770eedffe6
Huascar <huascarmc@hotmail.com>
2018-05-29 12:27:49 +0100

    connected image view to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var labelText: UILabel!
     @IBOutlet weak var labelTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
