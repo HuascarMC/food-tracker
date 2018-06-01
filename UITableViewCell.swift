79a6cf2e92213020bc8b8434628184459713ccb0
Huascar <huascarmc@hotmail.com>
2018-06-01 19:27:47 +0100

    created MealTableViewCell

---
+++ UITableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  UITableViewCell.swift
+//  FoodTracker
+//
+//  Created by Huascar  Montero on 01/06/2018.
+//  Copyright © 2018 Huascar  Montero. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCell: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
