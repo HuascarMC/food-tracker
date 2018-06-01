a0fcdb6fc7828a90e30772cf37bea805b0442eed
Huascar <huascarmc@hotmail.com>
2018-06-01 21:32:21 +0100

    Added edit button provided by table view controller

--- MealTableViewController.swift
+++ MealTableViewController.swift
@@ -38,12 +38,15 @@ class MealTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
         loadSampleMeals()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     override func didReceiveMemoryWarning() {
