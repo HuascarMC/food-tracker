9d7672bb452c3f0e3f6f663316a8ae5348ee9fac
Huascar <huascarmc@hotmail.com>
2018-06-01 20:27:13 +0100

    Added prepare method to MealViewController when buttonSave pressed

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -5,13 +5,37 @@
 //  Created by Huascar  Montero on 27/05/2018.
 //  Copyright © 2018 Huascar  Montero. All rights reserved.
 //
-
+import os.log
 import UIKit
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var labelTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    /*
+     This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var meal: Meal?
+    
+    //MARK: Navigation
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
