21cf30077e31a0b598c84e7d86769dea1ec12a1e
Huascar <huascarmc@hotmail.com>
2018-06-01 21:30:34 +0100

    Cancel button now works when updating meals

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -20,7 +20,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     var meal: Meal?
     
     @IBAction func cancel(_ sender: Any) {
-         dismiss(animated: true, completion: nil)
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     //MARK: Navigation
     
