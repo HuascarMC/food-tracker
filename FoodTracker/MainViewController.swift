2a2b9849a01005e2823737172587f6f6eb343415
Huascar <huascarmc@hotmail.com>
2018-06-17 14:33:43 +0100

    barchart renders well

--- FoodTracker/MainViewController.swift
+++ FoodTracker/MainViewController.swift
@@ -12,10 +12,12 @@ import Charts
 
 class MainViewController: UIViewController {
     @IBOutlet weak var lineChart: LineChartView!
-    
-    
+    @IBOutlet weak var BarChart: BarChartView!
     
     var db: Firestore!
+    let months = ["Jan", "Feb", "Mar", "Apr", "May"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0]
+    let unitsBought = [10.0, 14.0, 60.0, 13.0, 2.0]
     var currentDate: Date?
     var yesterday: Date?
     var beforeYesterday: Date? {
@@ -43,7 +45,9 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
         visitorsByDay.removeAll()
         self.setDates()
+
     }
+    
     
     private func setDates() {
         let date = NSDate()
@@ -57,7 +61,108 @@ class MainViewController: UIViewController {
 //        updateLineChart()
 
         // Do any additional setup after loading the view.
+    
+    
+    //legend
+    let legend = BarChart.legend
+    legend.enabled = true
+    legend.horizontalAlignment = .right
+    legend.verticalAlignment = .top
+    legend.orientation = .vertical
+    legend.drawInside = true
+    legend.yOffset = 10.0;
+    legend.xOffset = 10.0;
+    legend.yEntrySpace = 0.0;
+    
+    
+    let xaxis = BarChart.xAxis
+    xaxis.drawGridLinesEnabled = true
+    xaxis.labelPosition = .bottom
+    xaxis.centerAxisLabelsEnabled = true
+    xaxis.valueFormatter = IndexAxisValueFormatter(values:self.months)
+    xaxis.granularity = 1
+    
+    
+    let leftAxisFormatter = NumberFormatter()
+    leftAxisFormatter.maximumFractionDigits = 1
+    
+    let yaxis = BarChart.leftAxis
+    yaxis.spaceTop = 0.35
+    yaxis.axisMinimum = 0
+    yaxis.drawGridLinesEnabled = false
+    
+    BarChart.rightAxis.enabled = false
+    //axisFormatDelegate = self
+    
+    setChart()
+}
+
+func setChart() {
+    BarChart.noDataText = "You need to provide data for the chart."
+    var dataEntries: [BarChartDataEntry] = []
+    var dataEntries1: [BarChartDataEntry] = []
+    
+    for i in 0..<self.months.count {
+        
+        let dataEntry = BarChartDataEntry(x: Double(i) , y: self.unitsSold[i])
+        dataEntries.append(dataEntry)
+        
+        let dataEntry1 = BarChartDataEntry(x: Double(i) , y: self.self.unitsBought[i])
+        dataEntries1.append(dataEntry1)
+        
+        //stack barchart
+        //let dataEntry = BarChartDataEntry(x: Double(i), yValues:  [self.unitsSold[i],self.unitsBought[i]], label: "groupChart")
+        
+        
+        
     }
+    
+    let chartDataSet = BarChartDataSet(values: dataEntries, label: "Male")
+    let chartDataSet1 = BarChartDataSet(values: dataEntries1, label: "Female")
+    
+    let dataSets: [BarChartDataSet] = [chartDataSet,chartDataSet1]
+    chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+    //chartDataSet.colors = ChartColorTemplates.colorful()
+    //let chartData = BarChartData(dataSet: chartDataSet)
+    
+    let chartData = BarChartData(dataSets: dataSets)
+    
+    
+    let groupSpace = 0.3
+    let barSpace = 0.05
+    let barWidth = 0.3
+    // (0.3 + 0.05) * 2 + 0.3 = 1.00 -> interval per "group"
+    
+    let groupCount = self.months.count
+    let startYear = 0
+    
+    
+    chartData.barWidth = barWidth;
+    BarChart.xAxis.axisMinimum = Double(startYear)
+    let gg = chartData.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+    print("Groupspace: \(gg)")
+    BarChart.xAxis.axisMaximum = Double(startYear) + gg * Double(groupCount)
+    
+    chartData.groupBars(fromX: Double(startYear), groupSpace: groupSpace, barSpace: barSpace)
+    //chartData.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+    BarChart.notifyDataSetChanged()
+    
+    BarChart.data = chartData
+    
+    
+    
+    
+    
+    
+    //background color
+    BarChart.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+    
+    //chart animation
+    BarChart.animate(xAxisDuration: 1.5, yAxisDuration: 1.5, easingOption: .linear)
+    
+    
+}
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
