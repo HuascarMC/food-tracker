c8269f3c1c93af3b67528f7829e8add5b943c943
Huascar <huascarmc@hotmail.com>
2018-06-25 16:53:21 +0100

    edit meal updates date pickers to current date

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -113,7 +113,10 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
             labelTextField.text   = meal.name
             photoImageView.image = meal.photo
             ratingControl.rating = meal.rating
+            startDatePicker.setDate(meal.startDate!, animated: true)
+            endDatePicker.setDate(meal.endDate!, animated: true)
         }
+        
         // [START setup]
         let settings = FirestoreSettings()
         settings.isPersistenceEnabled = true
